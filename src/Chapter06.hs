29ab7acd8a50de79b25cbaed083fcf825411e154
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-01-11 10:58:28 +0100

    Chapter06 done

---
+++ src/Chapter06.hs
@@ -0,0 +1,13 @@
+module Chapter06 where
+  -- Rank-N Types
+  -- Exercise 6.3-i
+  -- What is the rank of Int -> forall a. a -> a
+  -- Solution: Rank 1
+
+  -- Exercise 6.3-ii
+  -- What is the rank of (a -> b) -> (forall c. c -> a) -> b
+  -- Solution: Rank 2
+
+  -- Exercise 6.3-iii
+  -- What is the rank of ((forall x. m x -> b (z m x)) -> b (z m a))  -> m a
+  -- Solution: Rank 3
