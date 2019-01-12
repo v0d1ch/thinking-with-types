15363a4a6a06a0334b6fcfead7c95b1ec36b350e
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-01-12 23:16:33 +0100

    Cont Applicative instance

--- src/Chapter07.hs
+++ src/Chapter07.hs
@@ -1,9 +1,33 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE InstanceSigs #-}
 module Chapter07 where
 
 newtype Cont a = Cont
   { unCont :: forall r. (a -> r) -> r
   }
 
+-- Exercise 6.4-i
+-- Provide Functor instance for Cont
 instance Functor Cont where
+  fmap :: (a -> b) -> Cont a -> Cont b
   fmap f (Cont c) = Cont (\x -> c $ x . f)
+
+-- Exercise 6.4-ii
+-- Provide Applicative instance for Cont
+instance Applicative Cont where
+  pure :: a -> Cont a
+  pure c = Cont (\a -> a c)
+
+  (<*>) :: Cont (a -> b) -> Cont a -> Cont b
+  (<*>) (Cont f) (Cont a) =
+    -- Cont (\br -> br $ f a)
+    Cont $ \br ->
+      f $ \ab ->
+        a $ br . ab
+
+-- Exercise 6.4-iii
+-- Provide Monad instance for Cont
+instance Monad Cont where
+  return = pure
+  (>>=) :: Cont a -> (a -> Cont b) -> Cont b
+  Cont m >>= f = Cont $ undefined
